53029aa3a0b52a1a75a0b327405c58ee23a05b3e
Robert Faldo <robertfaldo@gmail.com>
2023-01-08 19:06:00 +0000

    add code from exercise 2

---
+++ lib/formatter/phone_number.rb
@@ -0,0 +1,33 @@
+module Formatter
+  class PhoneNumber
+    class InvalidNumber < StandardError; end
+
+    VALID_PREFIXES = ["+44", "44", "0"]
+
+    def self.call(number)
+      raise InvalidNumber if number.empty?
+      raise InvalidNumber if valid_prefix(number).nil?
+
+      numb_without_spaces = number.gsub(" ", "")
+      num_without_prefix = numb_without_spaces.gsub(valid_prefix(number), "")
+
+      raise InvalidNumber unless only_contains_numbers?(num_without_prefix)
+      raise InvalidNumber unless num_without_prefix.length == 10
+      raise InvalidNumber unless num_without_prefix.start_with?("7")
+
+      "0#{num_without_prefix}"
+    end
+
+    private
+
+    def self.only_contains_numbers?(number)
+      number.scan(/\D/).empty?
+    end
+
+    def self.valid_prefix(number)
+      VALID_PREFIXES.find do |prefix|
+        number.start_with?(prefix)
+      end
+    end
+  end
+end
